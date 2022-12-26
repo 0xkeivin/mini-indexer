6ffa9fdc62e2a2f4f26b8d3508b277190f97d3fa
0xkeivin <keivincatwork@gmail.com>
2022-12-27 00:06:14 +0800

    polling works for latest N blocks

--- src/main.go
+++ src/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/joho/godotenv"
@@ -51,7 +53,7 @@ func goDotEnvVariable(key string) string {
 func pollEtherscan(url string) ([]byte, error) {
 	// Create a new HTTP client and set a timeout
 	client := &http.Client{
-		Timeout: time.Second * 5, // Set Timeout to 5 seconds
+		Timeout: time.Second * 10, // Set Timeout to 5 seconds
 	}
 	// Create a new request
 	req, err := http.NewRequest("GET", url, nil)
@@ -76,29 +78,75 @@ func pollEtherscan(url string) ([]byte, error) {
 	return body, nil
 
 }
-func main() {
-	etherscanAPIKey := goDotEnvVariable("ETHERSCAN_API_KEY")
-	// log.Infof("Using Etherscan API Key: %s", etherscanAPIKey)
-	// Using USDC contract address
-	address := "0x7EA2be2df7BA6E54B1A9C70676f668455E329d29"
-	// Base URL
-	baseUrl := `https://api.etherscan.io/api?module=logs&action=getLogs&page=1&offset=1000`
-	fullUrl := baseUrl + `&address=` + address + `&apikey=` + etherscanAPIKey
+
+type BlockResponse struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+	Result  string `json:"result"`
+}
+
+// function to get latest block number
+func getLatestBlockNumber(apiKey string) int {
+	// get current time
+	currentTime := time.Now().Unix()
+	log.Infof("Current time: %d", currentTime)
+	// get latest block number
+	baseUrl := `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=`
+	fullUrl := baseUrl + fmt.Sprint(currentTime) + `&closest=before&apikey=` + apiKey
 	log.Infof("Full URL: %s", fullUrl)
 	// Poll etherscan
 	resp, err := pollEtherscan(fullUrl)
 	if err != nil {
-		log.Info("Error polling etherscan: %s", err)
+		log.Infof("Error polling etherscan: %s", err)
 	}
 	// Convert response to JSON
-	convertByteToJSON(resp)
+	var blockResponse BlockResponse
+	err = json.Unmarshal(resp, &blockResponse)
+	if err != nil {
+		log.Infof("Error converting response to JSON: %s", err)
+	}
+	if blockResponse.Message != "OK" {
+		log.Infof("Error getting latest block number: %s", blockResponse.Message)
+	}
+	// Return latest block number
+	latestBlock, err := strconv.Atoi(blockResponse.Result)
+	if err != nil {
+		log.Infof("Error converting block number to int: %s", err)
+	}
+	return latestBlock
+
+}
+func main() {
+	etherscanAPIKey := goDotEnvVariable("ETHERSCAN_API_KEY")
+	// log.Infof("Using Etherscan API Key: %s", etherscanAPIKey)
+
+	// Using USDC contract address
+	address := "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+	// Base URL
+	baseUrl := `https://api.etherscan.io/api?module=logs&action=getLogs&page=1&offset=1000`
 
 	// for loop
-	ticker := time.Tick(10 * time.Second)
+	ticker := time.Tick(20 * time.Second)
 	for range ticker {
 		currentTime := time.Now()
 		// print log with tick time
-		log.Infof("%s - Tick at", currentTime.Format("2006-01-02 15:04:05"))
+		log.Infof("Tick at - %s", currentTime.Format("2006-01-02 15:04:05"))
+		// get latest block number
+		latestBlockNumber := getLatestBlockNumber(etherscanAPIKey)
+		fromBlock := latestBlockNumber - 10 // get last 10 blocks
+		log.Infof("Latest block number: %v", latestBlockNumber)
+		log.Infof("From block number: %v", fromBlock)
+
+		// fullUrl := baseUrl + `&address=` + address + `&apikey=` + etherscanAPIKey
+		fullUrl := baseUrl + `&address=` + address + `&fromBlock=` + fmt.Sprint(fromBlock) + `&toBlock=` + fmt.Sprint(latestBlockNumber) + `&apikey=` + etherscanAPIKey
+		log.Infof("Full URL: %s", fullUrl)
+		// Poll etherscan
+		resp, err := pollEtherscan(fullUrl)
+		if err != nil {
+			log.Info("Error polling etherscan: %s", err)
+		}
+		// Convert response to JSON
+		convertByteToJSON(resp)
 
 	}
 }
