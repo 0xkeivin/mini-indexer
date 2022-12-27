2ac4bf2d587de01a88044c5b96dbedbd4cfe3aac
0xkeivin <keivincatwork@gmail.com>
2022-12-27 16:04:13 +0800

    remove commented code

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -11,112 +11,8 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-// // Create a function that converts byte[] to JSON
-// func convertByteToJSON(body []byte) {
-// 	var data interface{}
-// 	err := json.Unmarshal(body, &data)
-// 	if err != nil {
-// 		log.Infof("Error converting byte to JSON: %s", err)
-// 		return
-// 	}
-// 	// Encode data as JSON
-// 	jsonData, err := json.MarshalIndent(data, "", " ")
-// 	if err != nil {
-// 		log.Infof("Error encoding data as JSON: %s", err)
-// 		return
-// 	}
-// 	// Write JSON to file
-// 	err = os.WriteFile("response.json", jsonData, 0644)
-// 	if err != nil {
-// 		log.Infof("Error writing JSON to file: %s", err)
-// 		return
-// 	}
-// }
-
-// use godot package to load/read the .env file and
-// return the value of the key
-// func goDotEnvVariable(key string) string {
-
-// 	// load .env file
-// 	err := godotenv.Load(".env")
-
-// 	if err != nil {
-// 		log.Fatalf("Error loading .env file")
-// 	}
-
-// 	return os.Getenv(key)
-// }
-
-// // Polls etherscan for new transactions records
-// func pollEtherscan(url string) ([]byte, error) {
-// 	// Create a new HTTP client and set a timeout
-// 	client := &http.Client{
-// 		Timeout: time.Second * 10, // Set Timeout to 5 seconds
-// 	}
-// 	// Create a new request
-// 	req, err := http.NewRequest("GET", url, nil)
-// 	if err != nil {
-// 		// return "", err
-// 		log.Info("Error creating request: %s", err)
-// 	}
-// 	// Set headers, if necessary
-// 	req.Header.Set("Content-Type", "application/json")
-// 	// Send the request
-// 	resp, err := client.Do(req)
-// 	if err != nil {
-// 		log.Info("Error sending request: %s", err)
-// 	}
-// 	// Read the response body
-// 	defer resp.Body.Close()
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		log.Info("Error reading response body: %s", err)
-// 	}
-// 	// Return the response body
-// 	return body, nil
-
-// }
-
-// type BlockResponse struct {
-// 	Status  string `json:"status"`
-// 	Message string `json:"message"`
-// 	Result  string `json:"result"`
-// }
-
-// // function to get latest block number
-// func getLatestBlockNumber(apiKey string) int {
-// 	// get current time
-// 	currentTime := time.Now().Unix()
-// 	log.Infof("Current time: %d", currentTime)
-// 	// get latest block number
-// 	baseUrl := `https://api.etherscan.io/api?module=block&action=getblocknobytime&timestamp=`
-// 	fullUrl := baseUrl + fmt.Sprint(currentTime) + `&closest=before&apikey=` + apiKey
-// 	log.Infof("Full URL: %s", fullUrl)
-// 	// Poll etherscan
-// 	resp, err := pollEtherscan(fullUrl)
-// 	if err != nil {
-// 		log.Infof("Error polling etherscan: %s", err)
-// 	}
-// 	// Convert response to JSON
-// 	var blockResponse BlockResponse
-// 	err = json.Unmarshal(resp, &blockResponse)
-// 	if err != nil {
-// 		log.Infof("Error converting response to JSON: %s", err)
-// 	}
-// 	if blockResponse.Message != "OK" {
-// 		log.Infof("Error getting latest block number: %s", blockResponse.Message)
-// 	}
-// 	// Return latest block number
-// 	latestBlock, err := strconv.Atoi(blockResponse.Result)
-// 	if err != nil {
-// 		log.Infof("Error converting block number to int: %s", err)
-// 	}
-// 	return latestBlock
-
-// }
 func main() {
 	log.Info("Starting web3-indexer...")
-	// etherscanAPIKey := goDotEnvVariable("ETHERSCAN_API_KEY")
 	etherscanAPIKey := env.GoDotEnvVariable("ETHERSCAN_API_KEY")
 
 	// log.Infof("Using Etherscan API Key: %s", etherscanAPIKey)
@@ -148,7 +44,6 @@ func main() {
 		}
 		// Convert response to JSON
 		utils.ConvertByteToJSON(resp)
-		// convertByteToJSON(resp)
 
 	}
 }
